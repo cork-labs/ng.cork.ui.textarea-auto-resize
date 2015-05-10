a9dde7c30fa9a9c18ccd51f1e40d66b179fd4496
Andre Torgal <andre.torgal@gmail.com>
2015-05-11 01:59:57 +0200

    chore(release): v0.0.7

--- dist/ng.cork.ui.textarea-auto-resize.js
+++ dist/ng.cork.ui.textarea-auto-resize.js
@@ -1,5 +1,5 @@
 /**
- * ng.cork.ui.textarea-auto-resize - v0.0.6 - 2015-04-17
+ * ng.cork.ui.textarea-auto-resize - v0.0.7 - 2015-05-11
  * https://github.com/cork-labs/ng.cork.ui.textarea-auto-resize
  *
  * Copyright (c) 2015 Cork Labs <http://cork-labs.org>
